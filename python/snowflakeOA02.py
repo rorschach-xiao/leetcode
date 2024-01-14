e11346832853c0309c5033681912bd17d8007c92
xiaoyang <xiao.yang3@northeastern.edu>
2024-01-13 23:32:38 -0800

    20240113 [snowflake OA]

--- python/snowflakeOA02.py
+++ python/snowflakeOA02.py
@@ -10,30 +10,26 @@ def getMinSwap(arr):
     for i in range(n):
         if arr[i] == i + 1 or (arr[i] == 0 and i >= MAX):
             continue
-        elif arr[i] == 0 and i < MAX:
-            arr[i] = i + 1
-            arr[pos_dict[i + 1]] = 0
-            ans += 1
         else:
             loop_set = set()
             cur = arr[i]
             while cur != 0 and cur not in loop_set:
                 loop_set.add(cur)
                 cur = arr[cur - 1]
-            if len(loop_set) == 1:
+            if cur == 0:
+                ans += len(loop_set)
+                for ele in loop_set:
+                    arr[ele - 1] = ele
+                arr[i] = 0
+                arr[i], arr[pos_dict[i + 1]] = arr[pos_dict[i + 1]], arr[i]
                 ans += 1
-                ele = loop_set.pop()
-                arr[pos_dict[ele]] = 0
-                arr[ele - 1] = ele
             else:
                 ans += len(loop_set) + 1
                 for ele in loop_set:
                     arr[ele - 1] = ele
-
     return ans
 
 
 if __name__ == '__main__':
-    arr = [3, 2, 1, 0, 0]
+    arr = [1, 0, 0, 3, 2]
     print(getMinSwap(arr))
-
